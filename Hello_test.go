306739cda91ed921d15051ae292dd69335a1f6d8
Ilya Petrenko <gr0x562@gmail.com>
2024-06-17 23:50:01 +0300

    add more hello any language

--- Hello_test.go
+++ Hello_test.go
@@ -2,11 +2,39 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-	want := "Hello world!"
-	got := Hello()
+func TestHello(t *testing.T) {
+	t.Run("Saying hello to people", func(t *testing.T) {
+		want := "Hello, Руслан"
+		got := Hello("Руслан", "")
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when parameter is empty", func(t *testing.T) {
+		want := "Hello, world"
+		got := Hello("", "")
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello spanish", func(t *testing.T) {
+		want := "Hola, Руслан"
+		got := Hello("Руслан", spanish)
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello franch", func(t *testing.T) {
+		want := "Bonjour, Руслан"
+		got := Hello("Руслан", franch)
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want) 
+		t.Errorf("got %q want %q", got, want)
 	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	englishHelloPrefix = "Hello, "
+	franchHelloPrefix  = "Bonjour, "
+	spanishHelloPrefix = "Hola, "
+	gawaiHelloPrefix   = "Aloha, "
+
+	spanish = "Spanish"
+	franch  = "Franch"
+	gawai   = "Gawai"
+)
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("", ""))
 }
 
-func Hello() string {
-	return "Hello world!"
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	return getGreetingPrefix(language) + name
 }
 
+func getGreetingPrefix(language string) string {
+	switch language {
+	case franch:
+		return franchHelloPrefix
+	case spanish:
+		return spanishHelloPrefix
+	case gawai:
+		return gawaiHelloPrefix
+	default:
+		return englishHelloPrefix
+	}
+}
