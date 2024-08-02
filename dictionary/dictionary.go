5775441d660803cda707eba4cacd75045f20fdd1
Ruslan Farhiullin <farhiullin2001@mail.ru>
2024-08-02 22:46:40 +0300

    learn maps 2

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,5 +1,7 @@
 package dictionary
 
+import "errors"
+
 func Search(s map[string]string, x string) string {
 	return s[x]
 }
@@ -8,4 +10,21 @@ type Dictionary map[string]string
 
 func (d Dictionary) Search(s string) string {
 	return d[s]
+}
+
+func (d Dictionary) Add(s, c string) error {
+	_, ok := d[s]
+	if ok {
+		return errors.New("error")
+	}
+	d[s] = c
+	return nil
+}
+
+func (d Dictionary) Update(s, c string) {
+	d[s] = c
+}
+
+func (d Dictionary) Delete(s string) {
+	delete(d, s)
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -43,4 +43,54 @@ func TestDictionary(t *testing.T) {
 			t.Errorf("want %q got %q", want, got)
 		}
 	})
+
+	t.Run("Add", func(t *testing.T) {
+		d := Dictionary{}
+		want := "this is test"
+
+		d.Add("test", "this is test")
+
+		got := d.Search("test")
+		if want != got {
+			t.Errorf("want %q got %q", want, got)
+		}
+	})
+
+	t.Run("Add with error", func(t *testing.T) {
+		d := Dictionary{
+			"test": "this is test",
+		}
+
+		err := d.Add("test", "this is new test")
+		if err == nil {
+			t.Errorf("want error but got nil")
+		}
+	})
+
+	t.Run("Update", func(t *testing.T) {
+		d := Dictionary{
+			"test": "this is test",
+		}
+		want := "this is new test"
+
+		d.Update("test", "this is new test")
+
+		got := d.Search("test")
+		if want != got {
+			t.Errorf("want %s got %s", want, got)
+		}
+
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		d := Dictionary{"test": "this is test"}
+		want := ""
+
+		d.Delete("test")
+
+		got := d.Search("test")
+		if want != got {
+			t.Errorf("want %q got %q", want, got)
+		}
+	})
 }
